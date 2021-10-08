e7d75e8a8410931d4faed9fdbb08d37feb10c6e4
umesh kadam <umeshk149@gmail.com>
2021-10-08 07:24:45 +0530

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
